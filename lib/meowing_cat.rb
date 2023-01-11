b6bfd9af4518a97ce8dddf5e947c2a67ba31ae86
+ <rodarte_sarah@yahoo.com>
2023-01-10 23:02:03 -0700

    commit tree a81c96c68aedd0b7ee29e892b4a457706900fc5a

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,16 @@
+require 'pry'
+
+class Cat
+    attr_accessor :name
+
+    def meow
+        puts "meow!"
+    end
+
+end
+
+koda = Cat.new
+koda.name = "Koda"
+koda.name
+koda.meow
 ## code your solution here. 
